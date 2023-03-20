3598523a0eb78d47d9c563b18cf46b42d8fbae4c
Al-Amin Firdows <alaminfirdows@gmail.com>
2023-03-20 23:26:38 +0600

    feat: add pdf generation

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/pdf', function () {
+    $pdf = PDF::loadView('pdf');
+
+    return $pdf->stream('pdf.pdf');
+    //return $pdf->download('pdf.pdf');
+});
